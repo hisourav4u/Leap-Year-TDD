9c250edc505c80ca8be2f911e09da89601bd5959
Sourav Kundu <idofsouravkundu@gmail.com>
2020-05-19 00:05:11 +0530

    Refactored leapYearChecker.java

--- leapYearChecker.java
+++ leapYearChecker.java
@@ -2,10 +2,19 @@
 public class leapYearChecker {
 
 	public int isLeapYear(int year) {
-		if((year%400)==0)
-			return 1;
-		else
-			return 0;
+		
+		if((year%400)==0)	
+			return 1;	//All years divisible by 400 ARE leap years
+		
+		else {
+			if((year%100)==0) {
+				return 0;	//All years divisible by 100 but not by 400 are NOT leap years
+			}
+			
+			else
+				return 0;
+		}
+			
 	}
 
 }
