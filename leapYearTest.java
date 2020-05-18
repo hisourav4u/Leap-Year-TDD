cca52efb08065cfc5d66e8eb04eabc3dd65633cf
Sourav Kundu <idofsouravkundu@gmail.com>
2020-05-18 22:37:11 +0530

    leapYearTest.java(JUnit test case file) created

---
+++ leapYearTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Sourav
+ *
+ */
+class leapYearTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
